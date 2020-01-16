f08be052b64797c3034072b7a646da51e8307ab1
wakakusa <a.beginner.of.kdd.english@gmail.com>
2020-01-17 00:24:50 +0900

    Added processing to download library file if library file does not exist

---
+++ deps/build.jl
@@ -0,0 +1,43 @@
+using BinaryProvider 
+
+# Parse some basic command-line arguments
+const verbose = "--verbose" in ARGS
+#const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
+
+if Sys.islinux()
+    prefix=joinpath(@__DIR__, "usr/lib/lib_lightgbm.so")
+elseif Sys.iswindows()
+    prefix=joinpath(@__DIR__, "usr/lib/lib_lightgbm.dll")
+elseif Sys.ismacos()
+    prefix=joinpath(@__DIR__, "usr/lib/lib_lightgbm.dylib")
+else
+    error("not matching OS")
+end
+
+products = [
+    LibraryProduct(prefix, ["lib_lightgbm"], :lib_lightgbm),
+]
+
+bin_prefix ="https://github.com/microsoft/LightGBM/releases/download/v2.3.1"
+download_info = Dict(
+    MacOS(:x86_64) => ("$bin_prefix/lib_lightgbm.dylib", "f55a5811b9ecdcc6356aaf7788582e74a246c44d75510ce21e1166bc33c0b752"),
+    Linux(:x86_64) => ("$bin_prefix/lib_lightgbm.so", "07abb99ff02d67f0899d4393e8e2e9932c610156fcb35aca86fb01a615b6783e"),
+    Windows(:x86_64) => ("$bin_prefix/lib_lightgbm.dll", "40d4b62c8bf7d583f511e3ae226f3e8179a6ae9f2bef8b34b3a6e02f71156b9b"),
+    )
+
+# First, check 
+unsatisfied = any(!satisfied(p; verbose=verbose) for p in products)
+dl_info = choose_download(download_info, platform_key_abi())
+if dl_info === nothing && unsatisfied
+    error("Your platform (\"$(Sys.MACHINE)\", parsed as \"$(triplet(platform_key_abi()))\") is not supported by this package!")
+end
+
+# download
+if unsatisfied || !isinstalled(dl_info...; prefix=prefix)
+    # Download and install binaries
+    dirpath=splitdir(prefix)[1]
+    mkdir(splitdir(dirpath)[1])
+    mkdir(dirpath)
+    download(dl_info[1],prefix)
+
+end
--- src/LightGBM.jl
+++ src/LightGBM.jl
@@ -11,8 +11,27 @@ function __init__()
             "Set this variable to point to the LightGBM directory prior to loading LightGBM.jl ",
             "(e.g. `ENV[\"LIGHTGBM_PATH\"] = \"../LightGBM\"`).")
     else
+        if isfile(LGBM_library)
+            include(joinpath(@__DIR__,"deps/build.jl"))
+        end
+
+        if Sys.islinux()
+            prefix=joinpath(@__DIR__, "usr/lib/lib_lightgbm.so")
+        elseif Sys.iswindows()
+            prefix=joinpath(@__DIR__, "usr/lib/lib_lightgbm.dll")
+        elseif Sys.ismacos()
+            prefix=joinpath(@__DIR__, "usr/lib/lib_lightgbm.dylib")
+        else
+            prefix=""
+        end
+
+        if isfile(prefix)
+            ENV["LIGHTGBM_PATH"] = prefix
+        end
+
         global LGBM_library = Libdl.find_library(["lib_lightgbm.so", "lib_lightgbm.dll",
             "lib_lightgbm.dylib"], [ENV["LIGHTGBM_PATH"]])
+
         if LGBM_library == ""
             error("Could not open the LightGBM library at $(ENV["LIGHTGBM_PATH"]). ",
                   "Set this variable to point to the LightGBM directory prior to loading LightGBM.jl ",
